72df6fd7f6bb3b91806c8091be9a15d5f6b66c5e
dzlxt <7373309@qq.com>
2019-09-11 14:31:06 +0800

    03_lxt

---
+++ pages/assembly/mycpn.js
@@ -0,0 +1,23 @@
+// pages/assembly/mycpn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    logoname:"达州手艺人"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
